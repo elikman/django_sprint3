fb237604ab9259c0353f83b231e930818864a1e1
Eltaj Nabiev <eltajnabiev464@gmail.com>
2024-04-23 20:22:15 +0300

    Директория .vscode добавлена в gitignore

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,78 +1,33 @@
-from collections import defaultdict
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 from django.http import Http404
-
-Post = dict[str, any]
-PostsList = list[Post]
-
-posts: PostsList = [
-    {
-        'id': 0,
-        'location': 'Остров отчаянья',
-        'date': '30 сентября 1659 года',
-        'category': 'travel',
-        'text': '''Наш корабль, застигнутый в открытом море
-                страшным штормом, потерпел крушение.
-                Весь экипаж, кроме меня, утонул; я же,
-                несчастный Робинзон Крузо, был выброшен
-                полумёртвым на берег этого проклятого острова,
-                который назвал островом Отчаяния.''',
-    },
-    {
-        'id': 1,
-        'location': 'Остров отчаянья',
-        'date': '1 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Проснувшись поутру, я увидел, что наш корабль сняло
-                с мели приливом и пригнало гораздо ближе к берегу.
-                Это подало мне надежду, что, когда ветер стихнет,
-                мне удастся добраться до корабля и запастись едой и
-                другими необходимыми вещами. Я немного приободрился,
-                хотя печаль о погибших товарищах не покидала меня.
-                Мне всё думалось, что, останься мы на корабле, мы
-                непременно спаслись бы. Теперь из его обломков мы могли бы
-                построить баркас, на котором и выбрались бы из этого
-                гиблого места.''',
-    },
-    {
-        'id': 2,
-        'location': 'Остров отчаянья',
-        'date': '25 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Всю ночь и весь день шёл дождь и дул сильный
-                порывистый ветер. 25 октября.  Корабль за ночь разбило
-                в щепки; на том месте, где он стоял, торчат какие-то
-                жалкие обломки,  да и те видны только во время отлива.
-                Весь этот день я хлопотал  около вещей: укрывал и
-                укутывал их, чтобы не испортились от дождя.''',
-    },
-]
-
-POSTS_DICT: dict[int, Post] = {post['id']: post for post in posts}
-
-category_to_posts: defaultdict[str, list[Post]] = defaultdict(list)
-for post in posts:
-    category_to_posts[post['category']].append(post)
+from .models import Post, Category
 
 
 def index(request):
     """Главная страница / Лента записей"""
-    context = {'posts': reversed(posts)}
-    return render(request, 'blog/index.html', context)
+    posts = Post.objects.filter(
+        pub_date__lte=timezone.now(),
+        is_published=True,
+        category__is_published=True
+    ).select_related('category').order_by('-pub_date')[:5]
+    return render(request, 'blog/index.html', {'posts': posts})
 
 
 def post_detail(request, id: int):
     """Отображение полного описания выбранной записи"""
-    post = POSTS_DICT.get(id)
-    if post is None:
-        raise Http404('Вы указали неверный id')
-    context = {'post': post}
-    return render(request, 'blog/detail.html', context)
+    post = get_object_or_404(Post, id=id, is_published=True)
+    if post.category is not None and not post.category.is_published:
+        raise Http404("Post is not available")
+    return render(request, 'blog/detail.html', {'post': post})
 
 
 def category_posts(request, category_slug: str):
     """Отображение публикаций категории"""
-    sorted_posts = category_to_posts[category_slug]
-    context = {'category': category_slug, 'posts': sorted_posts}
-    return render(request, 'blog/category.html', context)
+    category = get_object_or_404(Category, slug=category_slug, is_published=True)
+    posts = Post.objects.filter(
+        category=category,
+        is_published=True,
+        pub_date__lte=timezone.now(),
+    ).select_related('category').order_by('-pub_date')
+    return render(request, 'blog/category.html', {'category': category, 'posts': posts})
